f7c845e818d3ca33e2b764f1aa8062a43ed0dd9b
GonzaloCedillo <goncedillo@gmail.com>
2019-01-26 12:33:45 +0100

    Script para producción

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 # UPDATE SYSTEM
 sudo apt-get update
-sudo apt-get install curl
+sudo apt-get install curl git -y
 
 # INSTALL NODEJS
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
